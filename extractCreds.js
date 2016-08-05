0b35764f2d88f0e2b65d29b8e647d440189a3120
Avi Cavale <avi@shippable.com>
2016-08-05 15:31:26 -0700

    adding docker login

--- extractCreds.js
+++ extractCreds.js
@@ -12,3 +12,7 @@ function getValuesFromIntegrationJson(formJSONValues) {
 
 var jsonValues = getValuesFromIntegrationJson(JSON.parse(process.argv[2]));
 console.log('docker login  -u "' + jsonValues.username + '" -p "' + jsonValues.password + '"');
+
+
+echo '{"channel": "youtube", "profile_type": "video", "member_key": "hello"}'
+| jq '{channel: (.profile_type + "." + .channel)}'
